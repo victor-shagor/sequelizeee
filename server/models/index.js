b160f9355de586a711cb0b7ea4630fd46cebdb99
victor-shagor <ojoabiolavictor@gmail.com>
2019-08-10 16:48:13 +0100

    we back he

--- server/models/index.js
+++ server/models/index.js
@@ -17,7 +17,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
-    config.url
+    config.url, config
   );
 }
 
